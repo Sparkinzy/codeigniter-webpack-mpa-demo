93d5fce1d051d4a324a67d7d54d35288f4219c63
Mu <sparkinzy@163.com>
2019-10-11 18:11:29 +0800

    ci3.10+smarty3.1.33+webpack4+mpa+templates

---
+++ application/helpers/output_helper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mu
+ * Date: 2019-10-11
+ * Time: 17:54
+ */
+
+if(!function_exists('view')){
+	/**
+	 * @param string $template 句点分割，从模版目录开始找模版文件 user.login => /user/login.html
+	 * @param array  $data 只想模版的参数
+	 * @param bool   $return 编译后的模版数据
+	 *
+	 * @return mixed
+	 */
+	function view($template='',$data= array(),$return = FALSE)
+	{
+		if (strrpos('html',$template)=== 0){
+			$template = join(DIRECTORY_SEPARATOR,explode('.',$template)).'.html';
+		}
+		$ci = & get_instance();
+		return $ci->smarty->view($template,$data,$return);
+	}
+}
